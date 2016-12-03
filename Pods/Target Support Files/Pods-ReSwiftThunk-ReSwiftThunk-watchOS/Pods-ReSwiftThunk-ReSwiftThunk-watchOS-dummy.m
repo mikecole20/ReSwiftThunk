cd7ad47cb226300a513b6ec4ea80767ebff7d849
Christian Tietze <christian.tietze@gmail.com>
2016-12-03 14:36:52 +0100

    add Pod dependencies for all platforms

---
+++ Pods/Target Support Files/Pods-ReSwiftThunk-ReSwiftThunk-watchOS/Pods-ReSwiftThunk-ReSwiftThunk-watchOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReSwiftThunk_ReSwiftThunk_watchOS : NSObject
+@end
+@implementation PodsDummy_Pods_ReSwiftThunk_ReSwiftThunk_watchOS
+@end
